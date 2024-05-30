39b82189f55a7d52c63209c3ae528cfd4b76a056
andrey <vslom03@mail.ru>
2024-05-30 23:23:33 +0300

    modified recommendations add feed of the week

--- RecSystem/recsystem.py
+++ RecSystem/recsystem.py
@@ -1,20 +1,24 @@
-from sqlalchemy import func
-from sqlalchemy.orm import relationship, sessionmaker, DeclarativeBase, Session
+from sqlalchemy import select, func, desc, exists, and_, Column, String, Integer, DateTime, Table, MetaData
+from sqlalchemy.future import select
+from typing import List, Optional
+from sqlalchemy.orm import Session
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from fastapi import FastAPI, HTTPException, Depends
-from datetime import datetime
 from database.pictures import Picture
 from database.users import User
 from database.collections import Collection
 from database.tags import Tag
 from database.likes import Like
+from database.col_to_pic_enrol import CollectionToPictureEnrollment
+from database.tag_to_pic_enrol import TagToPictureEnrollment
 from database.db import get_db
 from database.db import init_db
+from pydantic import BaseModel
 import uvicorn
-import random
+import datetime
+import sqlalchemy as sa
 
-init_db()
 
 # Функция для получения рекомендованных постов
 def get_recommendations(user_id, num_recommendations, db):
@@ -76,9 +80,7 @@ def get_recommendations(user_id, num_recommendations, db):
                 fix_list = [random_pictures[index].id for index in recommended_indices]
 
 
-    # Возвращаем рекомендованные посты
-
-    recommended_posts = [random_pictures[index].path for index in recommended_indices]
+    recommended_posts = [random_pictures[index] for index in recommended_indices]
 
     db.close()
     return recommended_posts[:num_recommendations]
@@ -87,15 +89,91 @@ def get_recommendations(user_id, num_recommendations, db):
 # Создание экземпляра FastAPI
 app = FastAPI()
 
-# Маршрут для добавления лайка
+class PictureResponse(BaseModel):
+    id: int
+    path: str
+    is_liked_by_user: bool
+    tags: List[int]
 
+    class Config:
+        orm_mode = True
 
 # Маршрут для получения рекомендаций
 # по дате добавления рекомендовать
-@app.post("/recommendations/")
+@app.post("/recommendations/", response_model=List[PictureResponse])
 async def get_recommendations_endpoint(user_id: int, num_recommendations: int, db: Session = Depends(get_db)):
-    recommendations = get_recommendations(user_id, num_recommendations, db)
-    if not recommendations:
+    recommended_posts = get_recommendations(user_id, num_recommendations, db)
+    picture_ids = [pic.id for pic in recommended_posts]
+    tags = db.query(Tag.id, TagToPictureEnrollment.picture_id).join(TagToPictureEnrollment, Tag.id == TagToPictureEnrollment.tag_id)\
+            .filter(TagToPictureEnrollment.picture_id.in_(picture_ids)).all()
+    
+    tags_dict = {}
+    for tag_id, picture_id in tags:
+        if picture_id not in tags_dict:
+            tags_dict[picture_id] = []
+        tags_dict[picture_id].append(tag_id)
+
+    response = []
+    for picture in recommended_posts:
+        response.append(PictureResponse(
+            id=picture.id,
+            path=picture.path,
+            is_liked_by_user=False,
+            tags=tags_dict.get(picture.id, [])
+        ))
+    
+    if not recommended_posts:
         raise HTTPException(status_code=404, detail="Recommendations not found")
-    return {"recommendations": recommendations}
-# uvicorn.run(app)
+    return response
+
+
+
+@app.get("/top_pictures", response_model=List[PictureResponse])
+def get_top_pictures(user_id: int, db: Session = Depends(get_db)):
+    one_week_ago = datetime.datetime.utcnow() - datetime.timedelta(weeks=1)
+    
+    # Подзапрос для проверки, лайкнул ли пользователь картинку
+    is_liked_subquery = db.query(Like.picture_id)\
+                          .filter(Like.user_id == user_id)\
+                          .subquery()
+    
+    # Запрос для получения топ 50 картинок с наибольшим числом лайков за последнюю неделю
+    top_pictures = db.query(
+        Picture.id,
+        Picture.path,
+        func.count(Like.picture_id).label("like_count"),
+        (Picture.id.in_(is_liked_subquery)).label("is_liked_by_user")
+    ).join(Like, Picture.id == Like.picture_id)\
+     .filter(Like.like_date >= one_week_ago)\
+     .group_by(Picture.id)\
+     .order_by(desc("like_count"))\
+     .limit(50)\
+     .all()
+    
+    # Получение тегов для каждой картинки
+    picture_ids = [pic.id for pic in top_pictures]
+    tags = db.query(Tag.id, TagToPictureEnrollment.picture_id).join(TagToPictureEnrollment, Tag.id == TagToPictureEnrollment.tag_id)\
+            .filter(TagToPictureEnrollment.picture_id.in_(picture_ids)).all()
+    
+    tags_dict = {}
+    for tag_id, picture_id in tags:
+        if picture_id not in tags_dict:
+            tags_dict[picture_id] = []
+        tags_dict[picture_id].append(tag_id)
+
+    # Формирование списка картинок для ответа
+    response = []
+    for picture in top_pictures:
+        response.append(PictureResponse(
+            id=picture.id,
+            path=picture.path,
+            is_liked_by_user=picture.is_liked_by_user,
+            tags=tags_dict.get(picture.id, [])
+        ))
+    
+    return response
+
+
+init_db()
+
+uvicorn.run(app)
