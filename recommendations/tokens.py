74fe41f7c4aae3348a25fd2bc637ebe4caa7f76d
andrey <vslom03@mail.ru>
2024-06-01 18:56:20 +0300

    Переместил файлы из RecSystem в recommendations

---
+++ recommendations/tokens.py
@@ -0,0 +1,6 @@
+def aws_access_key():
+    aws_access_key_id = 'YCAJEiaPqlOWX3O3WQV4SnVhw'
+    return aws_access_key_id
+def aws_secret_access():
+    aws_secret_access_key = 'YCMBZHwrafWkCWw5cZ_3h4N7jCVWJmAYKMbXZ6ak'
+    return aws_secret_access_key
